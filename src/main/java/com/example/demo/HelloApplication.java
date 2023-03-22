383858c6aba2ba34ed9aa897e9ceed784fb7c93a
Nikita <sorokin.nikita2003@yandex.ru>
2023-03-22 19:09:06 +0300

    first frame

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+//        Group ships = new Group();
+//        BorderPane root = new BorderPane();
+//        BorderPane.setAlignment(root, Pos.CENTER);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
