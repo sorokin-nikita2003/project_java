f3705250b8cec2d76aca809b480a3b5641deb51e
Nikita <sorokin.nikita2003@yandex.ru>
2023-03-24 22:22:04 +0300

    full layout

--- src/main/java/com/example/demo/frame_1Controller.java
+++ src/main/java/com/example/demo/frame_1Controller.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import  javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -32,6 +38,59 @@ public class frame_1Controller {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
+    }
+    @FXML
+    private void handleButtonClick_2(ActionEvent event) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("frame_2.fxml"));
+        Scene scene = new Scene(pane);
+//        scene.setCursor(Cursor.HAND);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    private void handleButtonClick_3(ActionEvent event) {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+
+        Pane pane = new Pane();
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 600,100);
+
+        Label label = new Label("Вы уверены, что хотите выйти?");
+        label.setAlignment(Pos.CENTER);
+        label.setStyle("-fx-font-size:40;-fx-font-size:40");
+
+        Button btn_no = new Button("Нет");
+        btn_no.setLayoutX(100);
+        btn_no.setLayoutY(60);
+
+
+        Button btn_yes = new Button("Да");
+        btn_yes.setLayoutX(400);
+        btn_yes.setLayoutY(60);
+        btn_yes.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                javafx.application.Platform.exit();
+            }
+        });
+        btn_no.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                window.close();
+            }
+        });
+
+
+        root.getChildren().add(label);
+        root.getChildren().add(btn_yes);
+        root.getChildren().add(btn_no);
+
+        window.setScene(scene);
+        window.showAndWait();
     }
 
 //    @FXML
--- src/main/java/com/example/demo/frame_3Controller.java
+++ src/main/java/com/example/demo/frame_3Controller.java
@@ -2,14 +2,18 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -26,12 +30,52 @@ public class frame_3Controller {
 
         Pane pane = new Pane();
 
+
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 600,100);
+
         Label label = new Label("Вы уверены, что хотите выйти?");
+        label.setAlignment(Pos.CENTER);
         label.setStyle("-fx-font-size:40;-fx-font-size:40");
 
-        Button btn = new Button("Вы уверены, что хотите выйти?");
+        Button btn_no = new Button("Нет");
+        btn_no.setLayoutX(100);
+        btn_no.setLayoutY(60);
 
-        Scene scene = new Scene(label, 600,100);
+
+        Button btn_yes = new Button("Да");
+        btn_yes.setLayoutX(400);
+        btn_yes.setLayoutY(60);
+        btn_yes.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Parent pane = null;
+                try {
+                    pane = FXMLLoader.load(getClass().getResource("frame_1.fxml"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                Scene scene = new Scene(pane);
+//        scene.setCursor(Cursor.HAND);
+                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                window.setScene(scene);
+                Stage stage = (Stage) btn1.getScene().getWindow();
+                stage.close();
+                window.show();
+            }
+        });
+        btn_no.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                window.close();
+            }
+        });
+
+
+        root.getChildren().add(label);
+        root.getChildren().add(btn_yes);
+        root.getChildren().add(btn_no);
 
         window.setScene(scene);
         window.showAndWait();
