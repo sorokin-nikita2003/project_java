82db5f9e60e4f6014c1dac39b8824fc0b7265ea3
oktawel <94171516+oktawel@users.noreply.github.com>
2023-04-28 20:03:00 +0300

    v0.1

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -10,8 +10,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -33,77 +34,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
-
-
-
-
-        final Random random = new Random();
-        int[][] player1 = new int[10][10];
-        int[][] player2 = new int[10][10];
-        int x = random.nextInt(9);
-        int y = random.nextInt(9);
-        player2[x][y] = 4;
-        int ship = 4;
-        String move2 = "";
-        //System.out.println(Arrays.deepToString(player2));
-        int move =  random.nextInt(3);
-            try {
-                switch (move) {
-                    case (0):
-                        player2[x + 1][y] = 4;
-                        move2 = "right";
-                        x = x + 1;
-                        break;
-                    case (1):
-                        player2[x - 1][y] = 4;
-                        move2 = "left";
-                        x = x - 1;
-                        break;
-                    case (2):
-                        player2[x][y + 1] = 4;
-                        move2 = "up";
-                        y = y + 1;
-                        break;
-                    case (3):
-                        player2[x][y - 1] = 4;
-                        move2 = "down";
-                        y = y - 1;
-                        break;
-                }
-            } catch (Exception e) {
-                switch (move) {
-                    case (0):
-                        player2[x - 1][y] = 4;
-                        move2 = "left";
-                        x = x - 1;
-                        break;
-                    case (1):
-                        player2[x + 1][y] = 4;
-                        move2 = "right";
-                        x = x + 1;
-                        break;
-                    case (2):
-                        player2[x][y - 1] = 4;
-                        move2 = "down";
-                        y = y - 1;
-                        break;
-                    case (3):
-                        player2[x][y + 1] = 4;
-                        move2 = "up";
-                        y = y + 1;
-                        break;
-                }
-            }
-
-        PrintArray(player2);
-    }
-    private static void PrintArray(int[][] Array){
-        for (int i = 0; i < 10; i++) {  //идём по строкам
-            for (int j = 0; j < 10; j++) { //идём по столбцам
-                System.out.print(" " + Array[i][j] + " "); //вывод элемента
-            }
-            System.out.println();//перенос строки ради визуального сохранения табличной формы
-        }
+        //launch();
+        Logic logic = new Logic();
+        logic.generate_ships();
     }
 }
